d48f20c8730818e42e4424b3deca5876003dbb86
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-12 09:03:17 +0530

    Adding Duplicate Values

---
+++ EmployeePayroll.sql
@@ -0,0 +1,20 @@
+------- UC 9: Rename Salary to Basic Pay and Add Deduction,Taxable pay, Income Pay , Netpay -------
+
+EXEC sp_RENAME 'empoyee_payroll.Basic Pay' , 'BasicPay', 'COLUMN'
+Alter table empoyee_payroll
+add Deduction float,TaxablePay float, IncomeTax float,NetPay float;
+Update empoyee_payroll 
+set Deduction=1000
+where Gender='F';
+Update empoyee_payroll 
+set Deduction=2000
+where Gender='M';
+update empoyee_payroll
+set NetPay=(BasicPay - Deduction)
+update empoyee_payroll
+set TaxablePay=0,IncomeTax=0
+select * from empoyee_payroll;
+------- UC 10: Adding another Value for Rujula in Editing Department -------
+
+Insert into empoyee_Payroll(name,BasicPay,StartDate,Address,EmployeePhoneNumber,EmployeeDepartment) values ('Gayathri',250000,'2019-04-20','Chennai,TN','9600054540','Editing');
+select * from empoyee_payroll;
