19709a55d3b7b313a2bb4f54c0e95f9bf6791d59
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-12 08:53:48 +0530

    Inserting Values

---
+++ EmployeePayroll.sql
@@ -0,0 +1,7 @@
+--UC-3 Insert values in Table
+Insert into empoyee_payroll(name,salary,startDate) values
+('shalini',20000,'2021-03-12'),
+('Magesh',25000,'2021-04-18'),
+('Gayathri',10000,'2020-05-13'),
+('Aruna',30000,'2020-08-19');
+select * from empoyee_payroll;
